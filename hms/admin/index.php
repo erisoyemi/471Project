0858cdb49e8bb9f959d113e61457401337e69abf
erisoyemi <erisoyemi@gmail.com>
2022-12-06 15:12:50 -0700

    updates

---
+++ hms/admin/index.php
@@ -0,0 +1,39 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    
+    <title></title>
+</head>
+<body>
+    <?php
+    include("../include/header.php");
+    ?>
+<div class='container-fluid'>
+    <div class='col-md-12'>
+        <div class="row">
+            <div class="col-md-2" style="margin-left: -10px;">
+
+                <div class="list-group">
+                    <a href="" class="list-group-item bg-info list-group-item-action">Dashboard</a>
+                </div>
+                <div class="list-group">
+                    <a href="" class="list-group-item bg-info list-group-item-action">Admins</a>
+                </div>
+                <div class="list-group">
+                    <a href="" class="list-group-item bg-info list-group-item-action">Doctors</a>
+                </div>
+                <div class="list-group">
+                    <a href="" class="list-group-item bg-info list-group-item-action">Users</a>
+                </div>
+
+            </div>
+        </div>
+
+</div>
+
+</body>
+</html>
